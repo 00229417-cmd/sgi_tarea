ad51cf7b2943d99679548e14d002e1cf07c2c957
00229417-cmd <00229417@uca.edu.sv>
2025-11-02 17:09:36 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -3,7 +3,7 @@ import streamlit as st
 import mysql.connector
 
 def obtener_conexion():
-    cfg = st.secrets["db"]
+    cfg = st.secrets["db"]  # lee tus datos desde Secrets
     return mysql.connector.connect(
         host=cfg["host"],
         user=cfg["user"],
